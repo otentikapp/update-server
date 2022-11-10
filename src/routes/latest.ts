fcb576b85e56a09b73cdc8108b4b93c861b71989
Aris Ripandi <riipandi@gmail.com>
2022-11-10 23:23:53 +0700

    add signature

--- src/routes/latest.ts
+++ src/routes/latest.ts
@@ -3,19 +3,44 @@ import { Env } from '../interface'
 import { prettyJSON } from '../middleware'
 import { getLatestRelease } from '../utils/repository'
 
+async function getTextFromStream(readableStream: any) {
+    let reader = readableStream.getReader()
+    let utf8Decoder = new TextDecoder()
+    let nextChunk
+
+    let resultStr = ''
+
+    while (!(nextChunk = await reader.read()).done) {
+        let partialData = nextChunk.value
+        resultStr += utf8Decoder.decode(partialData)
+    }
+
+    return resultStr
+}
+
 const router = new Hono<{ Bindings: Env }>()
 router.use('*', prettyJSON())
 
 router.get('/', async (c) => {
     const latest = await getLatestRelease(c)
     const version = latest.tag_name.replace('v', '')
-    const files = latest.assets.map((item) => ({
-        name: item.name,
-        content_type: item.content_type,
-        download_url: item.browser_download_url,
-        download_count: item.download_count,
-        size: item.size,
-    }))
+
+    const files: any[] = await Promise.all(
+        latest.assets.map(async (item): Promise<any> => {
+            const sigUrl = `https://github.com/otentikapp/authenticator/releases/download/${latest.tag_name}/Authenticator.app.tar.gz.sig`
+            const resp: any = await fetch(sigUrl)
+            const signature = await resp.text()
+
+            return {
+                name: item.name,
+                content_type: item.content_type,
+                download_url: item.browser_download_url,
+                download_count: item.download_count,
+                size: item.size,
+                signature,
+            }
+        })
+    )
 
     return c.json({
         version,
--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -4,3 +4,32 @@ export default {
         github_repo: 'https://github.com/otentikapp/authenticator',
     },
 }
+
+export const aliases = {
+    darwin: ['mac', 'macos', 'osx'],
+    win32: ['windows32', 'windows', 'win'],
+    win64: ['windows64'],
+    // actually we bind all linux to appimage
+    appimage: ['linux32', 'linux64', 'linux'],
+    // Not supported yet
+    exe: ['exe'],
+    dmg: ['dmg'],
+    deb: ['debian'],
+    rpm: ['fedora'],
+}
+
+export const isSupportedPlatform = (platform: string) => {
+    if (typeof aliases[platform] !== 'undefined') {
+        return platform
+    }
+
+    for (const guess of Object.keys(aliases)) {
+        const list = aliases[guess]
+
+        if (list.includes(platform)) {
+            return guess
+        }
+    }
+
+    return false
+}
